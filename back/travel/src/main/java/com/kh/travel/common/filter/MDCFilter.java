15d69207adde26455355983f22022812193fe5bf
Ju Seongi <seongi0407@naver.com>
2024-10-12 09:34:26 +0900

    MDC Filter - 응답 정보 및 요청 처리 시간 측정 로깅 LoggingInterceptor - 요청 정보 로깅

--- back/travel/src/main/java/com/kh/travel/common/filter/MDCFilter.java
+++ back/travel/src/main/java/com/kh/travel/common/filter/MDCFilter.java
@@ -2,10 +2,14 @@ package com.kh.travel.common.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.util.StopWatch;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -15,13 +19,31 @@ import java.util.UUID;
 public class MDCFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        // 요청 처리 시간 측정 시작
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        // 캐싱을 위한 캐싱래퍼 선언
+        ContentCachingRequestWrapper cachingRequest = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        ContentCachingResponseWrapper cachingResponse = new ContentCachingResponseWrapper((HttpServletResponse) response);
+
+        // 랜덤 값으로 uuid 값 할당해서 로깅
         final UUID uuid = UUID.randomUUID();
-        System.out.println("MDCFilter.doFilter");
         MDC.put("request_id", uuid.toString());
-        HttpServletRequest httpReq = (HttpServletRequest) req;
-        log.info(httpReq.getParameter("userId"));
-        chain.doFilter(req, resp);
+
+        // 요청 처리 진행
+        chain.doFilter(cachingRequest, cachingResponse);
+
+        // 요청 처리 시간 측정 종료 및 response 정보 로깅
+        stopWatch.stop();
+        log.info(new String(cachingResponse.getContentAsByteArray()));
+        log.info(stopWatch.prettyPrint());
+
+        // response 정보 복사해서 클라이언트로 보내기
+        cachingResponse.copyBodyToResponse();
+
+        // MDC 비우기
         MDC.clear();
     } // doFilter
 } // class
